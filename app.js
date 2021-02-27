43d8f2255f852706ace11239c73d9a8c4ce22bf0
unknown <clapidas@gmail.com>
2021-02-27 07:36:24 -0500

    Switch to next js with tailwind

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const next = require('next')
+const PORT = process.env.PORT || 3000
+const dev = process.env.NODE_DEV !== 'production' //true false
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler() //part of next config
+
+nextApp.prepare().then(() => {
+    // express code here
+    const app = express()
+    app.get('*', (req,res) => {
+        return handle(req,res) // for all the react stuff
+    })
+    app.listen(PORT, err => {
+        if (err) throw err;
+        console.log(`ready at http://localhost:${PORT}`)
+    })
+})
