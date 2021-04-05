579b3e6d2eb49b334572f8340c28622560c65125
unknown <clapidas@gmail.com>
2021-04-05 19:35:58 -0400

    Improved bg svg

--- components/SocialMedia.js
+++ components/SocialMedia.js
@@ -1,7 +1,11 @@
 import styles from "../styles/SocialMedia.module.scss";
 
-const SocialMedia = ({ href, children }) => {
-  return <a href={href}>{children}</a>;
+const SocialMedia = ({ href, src }) => {
+  return (
+    <a href={href}>
+      <img className={styles.socialMediaIcon} src={src}></img>
+    </a>
+  );
 };
 
 export default SocialMedia;
