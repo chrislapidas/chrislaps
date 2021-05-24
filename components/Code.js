528a43c0cbbed559163e990276c1d3bf10227f9c
Christopher Lapidas <christopherlapidas@ChristophersAir.fios-router.home>
2021-05-24 16:33:48 -0400

    Finish codepen react article

--- components/Code.js
+++ components/Code.js
@@ -1,7 +1,11 @@
 import styles from "../styles/Code.module.scss";
 
 const Code = ({ children }) => {
-  return <div className={"elevation-2 " + styles.code}>{children}</div>;
+  return (
+    <div className={"elevation-2 " + styles.code}>
+      <code>{children}</code>
+    </div>
+  );
 };
 
 export default Code;
--- pages/articles/embed-codepen-in-react-jsx.js
+++ pages/articles/embed-codepen-in-react-jsx.js
@@ -37,9 +37,53 @@ export default function CodepenReact() {
         </div>
 
         <div className={styles.contentDiv}>
+          <Code>npm install --save react-codepen-embed</Code>
+        </div>
+
+        <div className={styles.contentDiv}>
+          Next let's hop over to Codepen and grab the username and hash
+          associated with our pen. The easiest way to do this is to open your
+          pen in the browser and check out the url, it should look something
+          like this:
+        </div>
+
+        <div className={styles.contentDiv}>
+          <b>https://codepen.io/clapidas/pen/PoppvOx</b>
+        </div>
+
+        <div className={styles.contentDiv}>
+          The "username" is simply your username which can be seen right after
+          the "codepen.io/" in the url, and the hash is the last part after the
+          last slash (in this case "Poppv0x").
+        </div>
+
+        <div className={styles.contentDiv}>
+          Now we can head back over to our React project and create the embedded
+          Codepen. Import the following into your react component file:
+        </div>
+
+        <div className={styles.contentDiv}>
+          <Code>import Codepen from "react-codepen-embed";</Code>
+        </div>
+
+        <div className={styles.contentDiv}>
+          Then go ahead and create the following wherever you want to create
+          your embedded Codepen:
+        </div>
+
+        <div className={styles.contentDiv}>
           <Code>
-            <code>npm install --save react-codepen-embed</code>
+            &lt;Codepen <br />
+            &nbsp; &nbsp; hash=&quot;PoppvOx&quot; <br />
+            &nbsp; &nbsp; user=&quot;clapidas&quot; <br />
+            &nbsp; &nbsp; loader=&#123;() =&gt;
+            &lt;div&gt;Loading...&lt;/div&gt;&#125; <br />
+            /&gt;
           </Code>
+        </div>
+
+        <div className={styles.contentDiv}>
+          And that's it! A simple, easy way to embed your Codepen in React JSX.
         </div>
       </article>
     </Article>
